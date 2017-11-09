0133ae1c2d870879432be80f3eddf0afe21405d5
LVPlum <2356674013@qq.com>
2017-11-09 08:52:00 +0800

    1109

---
+++ src/components/input-plate.vue
@@ -0,0 +1,56 @@
+<template id="input-plate">
+    <ul class="aui-list aui-media-list">
+        <li class="aui-list-item aui-padded-0 border-b">
+            <div class="aui-media-list-item-inner">
+                <div class="aui-list-item-inner aui-padded-l-15 aui-padded-r-0">
+                    <div class="aui-list-item-text">
+                        <div class="aui-list-item-title tea-text-blue aui-font-size-14">车牌号码</div>
+                    </div>
+                    <slot name="car_type"></slot>
+                    <div class="aui-info aui-padded-0 aui-margin-t-5">
+                        <slot name="vip"></slot>
+                        <div class="aui-list-item-label" style="width: 4rem;line-height: normal"  @click="openWinProvince">
+                            <span class="tea-text-title aui-font-size-16 tea-text-arrow-down">{{ province }}</span>
+                        </div>
+                        <div class="aui-list-item-input aui-font-size-16">
+                            <input type="text" placeholder="请输入车牌后5位数" maxlength="8"
+                                   v-model="value"
+                                   @input="updateValue($event.target.value)">
+                                   <!--:value="value"
+                                   @blur="updateValue($event.target.value)">-->
+                        </div>
+                        <div class="tea-text-round" style="border: none;"  @click="openWinPlate"><img src="../assets/select.png"></div>
+                        <!--<div class="tea-text-round tea-big"  @click="openWinPlate">选</div>-->
+                    </div>
+                </div>
+            </div>
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: 'input-plate',
+        template: '#input-plate',
+        props: ['province','value'],
+        data: function() {
+            return {
+            }
+        },
+        computed: {
+
+        },
+        methods: {
+            
+        },
+        watch: {
+
+        },
+        created: function(){
+            
+    };
+
+</script>
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import ETC from '../pages/etc.vue'
+import ETCTwo from '../pages/etc.vue'
 import VIP from '../pages/vip.vue'
 Vue.use(Router)
 
@@ -13,6 +14,14 @@ export default new Router({
                 title: '购买ETC'
             },
             component: ETC
+        },
+        {
+            path: '/etc',
+            name: 'etctwo',
+            meta: {
+                title: '购买ETC'
+            },
+            component: ETCTwo
         },
         {
             path: '/vip',
